81838fd43800ff87a0ff843631ccd9c994360e45
carelli91 <g.carelli@alumno.etec.um.edu.ar>
2025-04-22 12:55:58 -0300

    Paso 1

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -1,13 +1,32 @@
 
 package org.example
 
-class App {
-    val greeting: String
-        get() {
-            return "Gonzalo Carelli, 22/04/25"
-        }
+
+fun mostrarNombreyFecha() {
+    val nombre: String = "Gonza"
+    val fecha: String = "22/04/25"
+
+    println("Nombre: $nombre")
+    println("Fecha: $fecha")
 }
 
+
+fun mostrarTiposDeDatos() {
+    val nombre: String = "Gonza"
+    val edad: Int = 16
+    val altura: Double = 1.78
+    val esEstudiante: Boolean = true
+    
+    println("Nombre: $nombre")
+    println("Edad: $edad")
+    println("Altura: $altura")
+    println("Es estudiante: $esEstudiante")
+}
+
+
 fun main() {
-    println(App().greeting)
+    mostrarNombreyFecha()
+
+    mostrarTiposDeDatos()
+
 }
