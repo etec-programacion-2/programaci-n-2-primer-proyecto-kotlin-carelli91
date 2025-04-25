eb67ee309d5d119f5dfde0e5158fe67f165ffc57
gonza-carelli <g.carelli@alumno.etec.um.edu.ar>
2025-04-25 09:46:54 -0300

    Paso 2: Variables y tipos de datos

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -1,6 +1,15 @@
 
 package org.example
 
+//PASO 1
+fun mostrarNombreyFecha() {
+    val nombre: String = "Gonza"
+    val fecha: String = "22/04/25"
+
+    println("Nombre: $nombre")
+    println("Fecha: $fecha")
+}
+
 //PASO 2
 fun mostrarTiposDeDatos() {
     val nombre: String = "Gonza"
@@ -12,10 +21,17 @@ fun mostrarTiposDeDatos() {
     println("Edad: $edad")
     println("Altura: $altura")
     println("Es estudiante: $esEstudiante")
+
+    fun calcularAreaRectangulo(base: Double, altura: Double) {
+        val area = base * altura
+        println("El área del rectángulo es: $area")
+    }
 }
 
-
 fun main() {
+    //PASO 1
+    mostrarNombreyFecha()
+
     //PASO 2
     mostrarTiposDeDatos()
 
